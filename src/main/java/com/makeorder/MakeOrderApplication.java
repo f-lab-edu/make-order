b0135b55e86f7ec98a3dc83a1bf58a4439f815d2
depark <mem29238@gmail.com>
2024-11-23 17:24:06 +0900

    Init project

---
+++ src/main/java/com/makeorder/MakeOrderApplication.java
@@ -0,0 +1,13 @@
+package com.makeorder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MakeOrderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MakeOrderApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/makeorder/MakeOrderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.makeorder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MakeOrderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
